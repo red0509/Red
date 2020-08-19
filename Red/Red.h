9c2d35535add2fe9f26308d60931410d806daa56
Anton Pavlov <paapavlov@gmail.com>
2020-08-19 22:06:29 +0300

    first commit

---
+++ Red/Red.h
@@ -0,0 +1,19 @@
+//
+//  Red.h
+//  Red
+//
+//  Created by Anton Pavlov on 19.08.2020.
+//  Copyright © 2020 Anton Pavlov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Red.
+FOUNDATION_EXPORT double RedVersionNumber;
+
+//! Project version string for Red.
+FOUNDATION_EXPORT const unsigned char RedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Red/PublicHeader.h>
+
+
